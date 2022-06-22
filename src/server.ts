e059c03d722ba0d8699492399004789fb94369d6
Malcolm Lima <malcolmmellolima@gmail.com>
2022-06-22 10:29:34 -0300

    starting project and folders structures

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Olá, startup!")
+});
+
+app.listen(5000, () => {
+    console.log("Aplicação rodando na porta 5000");
+});
