57e15fcea378d07f322beba650c5de583648d766
Malcolm Lima <malcolmmellolima@gmail.com>
2022-07-15 17:54:43 -0300

    migrations

---
+++ src/db/migrations/1657917810165-CreateProducts.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class CreateProducts1657917810165 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "companies_products",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "company_id",
+                        type: "uuid"
+                    },
+                    {
+                        name: "product_name",
+                        type: "varchar"
+                    },
+                    {
+                        name: "front_cover",
+                        type: "varchar"
+                    },
+                    {
+                        name: "reviews",
+                        type: "float"
+                    },
+                    {
+                        name: "price",
+                        type: "float"
+                    }
+                ],
+                foreignKeys: [
+                    {
+                        name: "fk_companies",
+                        columnNames: ["company_id"],
+                        referencedTableName: "companies",
+                        referencedColumnNames: ["id"]
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("companies_products");
+    }
+
+}
